60e0b39b907f647028ac9126f6f1845f70a30cba
Ryan <glanzrm@gmail.com>
2022-04-28 10:20:50 -0500

    Enabled mode switching with state

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import Navbar from "./components/Navbar"
 import Main from "./components/Main"
 
 export default function App() {
+    const [darkMode, setDarkMode] = React.useState(false)
+
+    function toggleDarkMode() {
+        setDarkMode(prevDarkMode => !prevDarkMode)
+    }
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar
+                darkMode={darkMode}
+                toggleDarkMode={toggleDarkMode}
+            />
+            <Main
+                darkMode={darkMode}
+            />
         </div>
     )
 }
